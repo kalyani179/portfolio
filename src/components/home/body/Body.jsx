e420db66a182a0bbaf33b5d3b81646e58cf841c8
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-07-09 12:30:50 +0530

    navbar responsiveness done

--- src/components/home/body/Body.jsx
+++ src/components/home/body/Body.jsx
@@ -4,14 +4,14 @@ import { Bounce, Fade, Rotate, Zoom } from 'react-reveal';
 
 const Body = () => {
     return (<>
-        <div className="flex sm:flex-col md:flex-col sm:center md:center m-20 sm:m-3 md:m-8 space-x-28 sm:space-y-10 sm:space-x-0 md:space-x-0 md:space-y-14">
+        <div className="flex sm:flex-col md:flex-col sm:center md:center m-20 sm:m-2 md:m-8 space-x-28 sm:space-y-10 sm:space-x-0 md:space-x-0 md:space-y-14">
             <Bounce>
                 <img className="w-96 h-96 sm:w-72 sm:h-72 sm:center md:center md:w-80 md:h-80 bg-black/50 rounded-full" src={girlImage} alt="girl-image" />
             </Bounce>
             <div className="sm:center md:center sm:flex-col md:flex-col mt-20 space-y-1">
                 <Zoom duration={1500}>
                 <h1 className="text-[50px] sm:text-3xl md:text-3xl font-playfairdisplay italic font-bold md:w-full sm:w-full sm:text-center md:text-center">
-                    Hey, I'm <span className="gradient-text">Kalyani </span> Dantuluri.
+                    Hey, I'm <span className="gradient-text">Kalyani&nbsp;</span><span className="sm:hidden">Dantuluri.</span>
                 </h1>
                 </Zoom>
                 <Zoom duration={1500}>
--- src/components/home/navbar/NavBar.jsx
+++ src/components/home/navbar/NavBar.jsx
@@ -34,16 +34,23 @@ const NavBar = () => {
         <Navbar.Brand href="#home" className={"text-4xl sm:text-2xl md:text-3xl"}>Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className={isNavOpen ? 'open' : ''}>
             <span className="navbar-toggler-icon"></span>
-            <div className="absolute top-5 right-3">
-            <button className="lg:hidden" onClick={toggleNav}>
-                {isNavOpen ? <i className="fi fi-rr-cross-small"></i> : <i className="fi fi-br-menu-burger"></i>}
-            </button>
-        </div>
+            <div className="absolute top-5 right-3 sm:z-50">
+                <button className="lg:hidden" onClick={toggleNav}>
+                    {isNavOpen ? <i className="fi fi-rr-cross-small text-xl"></i> : <i className="fi fi-br-menu-burger sm:z-50"></i>}
+                </button>
+            </div>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className={`sm:hidden md:hidden me-auto border border-white border-opacity-25 rounded-3xl px-6 py-2`}>
+                <Nav.Link href="#home" className={activeLink === "home" ? "navbar-link active" : "navbar-link"} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                <Nav.Link href="#about" className={activeLink === "about" ? "navbar-link active" : "navbar-link"} onClick={() => onUpdateActiveLink('about')}>About</Nav.Link>
+                <Nav.Link href="#skills" className={activeLink === "skills" ? "navbar-link active" : "navbar-link"} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                <Nav.Link href="#projects" className={activeLink === "projects" ? "navbar-link active" : "navbar-link"} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                <Nav.Link href="#experience" className={activeLink === "experience" ? "navbar-link active" : "navbar-link"} onClick={() => onUpdateActiveLink('experience')}>Experience</Nav.Link>
+            </Nav>
         {
             isNavOpen && (
-            <Nav className={`me-auto border border-white border-opacity-25 rounded-3xl px-6 py-2 sm:absolute sm:right-0 sm:z-50 sm:py-10 sm:h-screen sm:border-none sm:grid sm:grid-rows-5 sm:bg-white/30`}>
+            <Nav className={`lg:hidden me-auto border border-white border-opacity-25 px-6 sm:absolute sm:top-0 sm:right-0 sm:z-30 sm:py-24 sm:pb-32 sm:pl-10 sm:h-full sm:border-none sm:grid sm:grid-rows-5 sm:bg-black/70`}>
                 <Nav.Link href="#home" className={activeLink === "home" ? "navbar-link active" : "navbar-link"} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
                 <Nav.Link href="#about" className={activeLink === "about" ? "navbar-link active" : "navbar-link"} onClick={() => onUpdateActiveLink('about')}>About</Nav.Link>
                 <Nav.Link href="#skills" className={activeLink === "skills" ? "navbar-link active" : "navbar-link"} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
